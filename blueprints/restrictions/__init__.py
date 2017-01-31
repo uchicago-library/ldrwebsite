92c130b5fdb003a4a1fa63f7cf5d7c45ffa35005
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-01-31 14:08:30 -0600

    added restrictions blueprint code

--- blueprints/restrictions/__init__.py
+++ blueprints/restrictions/__init__.py
@@ -1,12 +1,57 @@
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request
+from uuid import uuid4
+
+from .utils import new_rights_extension, get_ldr_object_filepath,\
+    get_premis_record, extract_record_rights, wrap_rightsExtensions,\
+    deactivate_group_of_rights_extensions
 
 RESTRICTION_CHANGE = Blueprint("restriction_change", __name__)
 
-@RESTRICTION_CHANGE.route("/restriction/select")
-def select():
-    return render_template("index.html")
+@RESTRICTION_CHANGE.route("/restrictions/", methods=["GET", "POST"])
+def select_an_object():
+    """a method to return a form for entering an object to change restrictions of
+    """
+    if request.method == "POST":
+        form = request.form
+        input_value = form.get("object-id").split("/")
+        accessionid = input_value[0]
+        objid = input_value[1]
+        return redirect("/ldrrestrictions/change/{}/{}".format(accessionid, objid))
+    else:
+        return render_template("front.html")
 
-@RESTRICTION_CHANGE.route("/restriction/change/<string:accession_id>")
-def change(accessionid):
-    return render_template("record.html")
+@RESTRICTION_CHANGE.route("/restrictions/change/<string:accessionid>/<string:objid>",
+                          methods=["GET", "POST"])
+def make_a_change(accessionid, objid):
+    """a method to process a form to change the active restriction on a particular object
+       and change existing restrictions to inactive
+
+    __Args__
+    1. accessionid (str): the identifier for a particular accession
+    2. objid (str): the identifier for a particular object
+    """
+    from flask import current_app
+    root_path = current_app.config.get("LIVEPREMIS_PATH")
+    path_to = get_ldr_object_filepath(accessionid, objid, root_path=root_path)
+    fpath = get_ldr_object_filepath(accessionid, objid)
+    record = get_premis_record(fpath)
+    current_restrictions = extract_record_rights(record)
+    current_restriction = ','.join(current_restrictions)
+    current_accession = accessionid
+    current_object = "{}/{}".format(current_accession, objid)
+    if request.method == "POST":
+        form = request.form
+        new_rights_info = new_rights_extension(uuid4().hex, current_object,
+                                               form.get("desired-restriciton"), True,
+                                               form.get("comment"))
+        rights_extensions = deactivate_group_of_rights_extensions(record)
+        rights_extensions.append(new_rights_info)
+        new_rights = wrap_rightsExtensions(rights_extensions)
+        record.add_rights(new_rights)
+        record.write_to_file(path_to)
+        return render_template("receipt.html", objectChanged=current_object,
+                               newRestriction=form.get("desired_restriction"))
+    else:
+        return render_template("changeform.html", objectToChange=current_object,
+                               currentRestriction=current_restriction)
---
+++ blueprints/restrictions/utils.py
@@ -0,0 +1,95 @@
+
+from os.path import exists, join
+
+from pypairtree.utils import identifier_to_path
+from pypremis.lib import PremisRecord
+from pypremis.nodes import Rights, RightsExtension
+from uchicagoldrtoolsuite.bit_level.lib.misc.premisextensionnodes import Restriction,\
+    RestrictedObjectIdentifier, RightsExtensionIdentifier
+
+def new_rights_extension(extension_id, restricted_object_id, restriction_code,
+                         restriction_active, restriction_comment):
+    """a function to generate a rightsExtension element with minimally required data points
+
+    __Args__
+    1. extension_id (str): the identifier for the rightsExtension element being created
+    2. restricted_object_id (str): the identifier of the object that this rightsExtension describes
+    3. restriction_code (str): a SPCL restriction code
+    4. restriction_active (str): True or False, a statement about whether the
+                                 restriction should be considered active or not
+    5. restriction_comment (str): an optional string describing what this restriction is about
+    """
+    rights_ext_id = RightsExtensionIdentifier("DOI", extension_id)
+    restricted_object = RestrictedObjectIdentifier("DOI", restricted_object_id)
+    restrict = Restriction(restriction_code, restriction_active, restricted_object)
+    if restriction_comment:
+        restrict.set_restrictionReason(restriction_comment.strip())
+    rights_extension = RightsExtension()
+    rights_extension.set_field("rightsExtensionIdentifier", rights_ext_id)
+    rights_extension.set_field("restriction", restrict)
+    return rights_extension
+
+def get_ldr_object_filepath(accessionid, objid, root_path=None):
+    """a function to return a valid path to a ldr premis live PREMIS record
+    """
+    if not root_path or not exists(root_path):
+        root_path = "/disk/0/repositoryCode/livePremis/objects"
+    accession_path = str(identifier_to_path(accessionid))
+    objid_path = str(identifier_to_path(objid))
+    path_to = join(root_path[0], accession_path,
+                   "arf/pairtree_root", objid_path, "arf", "premis.xml")
+    return path_to
+
+def get_premis_record(file_path):
+    """a function to return a PremisRecord object from a path to a record
+    """
+    try:
+        record = PremisRecord(frompath=file_path)
+    except Exception:
+        raise Exception()
+    return record
+
+def extract_record_rights(a_record):
+    """a function retrieve the rights information from a particular PREMIS record
+    """
+    current_restrictions = []
+    current_rights = precord.get_rights_list()
+    for right in current_rights:
+        extensions = right.get_rightsExtension()
+        for extension in extensions:
+            restriction = extension.get_field("restriction")
+            code = restriction[0].get_field("restrictionCode")
+            active = restriction[0].get_field("active")
+            active = active[0] if len(active) == 1 else None
+            code = code[0] if len(code) == 1 else None
+            if active == "True" and code is not None:
+                current_restrictions.append(code)
+    return current_restrictions
+
+def wrap_rightsExtensions(group_of_rights_extensions):
+    """a function to wrap a bunch of rights extension elements in a PREMIS rights element
+    """
+    return Rights(rightsExtension=group_of_rights_extensions)
+
+def deactivate_group_of_rights_extensions(a_record):
+    old_rights = a_record.get_rights_list()
+    output = []
+    for right in old_rights:
+        extensions = right.get_rightsExtension()
+        for extension in extensions:
+            new = deactivate_a_right_extension(extension)
+            output.append(new)
+    return output
+
+def deactivate_a_right_extension(an_extension):
+    extension_id = an_extension.get_field("rightsExtensionIdentifier")[0].\
+        get_field("rightsExtensionIdentifierValue")
+    restriction = an_extension.get_field("restriction")[0]
+    code = restriction.get_field("restrictionCode")[0]
+    restricted_object_id = restriction.get_field("restrictedObjectIdentifier")[0].\
+        get_field("restrictedObjectIdentifierValue")
+    try:
+        comment = restriction.get_field("restrictionComment")
+    except KeyError:
+        comment = None
+    return new_rights_extension(extension_id, restricted_object_id, code, False, comment)
